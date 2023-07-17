88f76126f64918c04f3406ec3f3c50e852e486c1
Al0cam <benjaminsikac@gmail.com>
2023-07-17 20:20:30 +0200

    Added axios to  fetch data from the backend and fetched data from the backend

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,36 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+import axios from 'axios';
+import { Task } from './models/Task';
+
+let tasks: Task[] = [];
+
+function getAllTasks(){
+  axios.get('http://localhost:3000/task')
+  .then((response) => {
+    response.data.forEach(element => {
+      tasks = [...tasks, new Task(element)]
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+}
+
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  
+  {#each tasks as task}
+    <div>
+      <h1>{task.title}</h1>
+      <p>{task.description}</p>
+    </div>
+    
+  {/each}
+ 
+  <button on:click={getAllTasks}> mr white je gay </button>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+
 </style>
